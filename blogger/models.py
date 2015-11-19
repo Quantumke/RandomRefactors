940e9801a18e317cd11f4018ddd63b716f1edf79
coder x <nguruben@gmail.com>
2015-11-20 01:01:38 +0300

    models and views working

--- blogger/models.py
+++ blogger/models.py
@@ -5,6 +5,6 @@ class Post(models.Model):
 	title = models.CharField(max_length=100, unique=False)
 	description= models.CharField(max_length=100, unique=False)
 	author = models.CharField(max_length=100, unique=False)
-	image=models.ImageField(max_lenght=100, unique=False)
+	image=models.ImageField()
 	def __unicode__(self):
 		return '%s' %self.title
