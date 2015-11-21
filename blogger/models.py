5105bcc90b9fe0c3435df206f8e2f8e847479ff0
Quantumke <nguruben@gmail.com>
2015-11-21 12:09:04 +0300

    now working

--- blogger/models.py
+++ blogger/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-<<<<<<< HEAD
 import operator
 
 
@@ -21,14 +20,3 @@ class Post(models.Model):
 		return '%s' %self.title
 
 
-=======
-
-# Create your models here
-class Post(models.Model):
-	title = models.CharField(max_length=100, unique=False)
-	description= models.CharField(max_length=100, unique=False)
-	author = models.CharField(max_length=100, unique=False)
-	image=models.ImageField()
-	def __unicode__(self):
-		return '%s' %self.title
->>>>>>> 8bb6879efff4d6b1dd8ec3b3ff5a48ee6000e3f8
--- blogger/views.py
+++ blogger/views.py
@@ -5,11 +5,8 @@ from blogger.models import Post
 
 def index(request):
 	return render_to_response('index.html',{
-<<<<<<< HEAD
+
 		'Post':Post.objects.filter(author='Ben'),
 		'cats':Post.objects.filter(group='Grapevine')
-               
-=======
-		'Post':Post.objects.all()[:5]
->>>>>>> 8bb6879efff4d6b1dd8ec3b3ff5a48ee6000e3f8
+		
 	})
