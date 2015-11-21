0a71a661be13510caa6a440f2a58d07153e3ef74
Quantumke <nguruben@gmail.com>
2015-11-21 12:42:47 +0300

    added to template

--- blogger/views.py
+++ blogger/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from blogger.models import Post
+from django.db.models import F
 
 # Create your views here.
 
@@ -7,7 +8,7 @@ def index(request):
 	return render_to_response('index.html',{
 
 		'Post':Post.objects.filter(author='Ben'),
-		'cats':Post.objects.filter(group='Grapevine')
-		'hit':Post.objects.filter(pk=1).update(counter=F('counter') + 1)
-		
+		'cats':Post.objects.filter(group='Grapevine'),
+		'hit':Post.objects.filter(pk=1).update(counter=F('counter') + 1),
+	        'popular':Post.objects.order_by('-counter')	
 	})
